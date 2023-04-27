466134f0b15f83db4a4660a79763e82fd47e4652
vitmcalhau <v248740@dac.unicamp.br>
2023-04-27 20:32:21 -0300

    cria funcao para realizar login com a conta do google

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign.dart';
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -13,6 +14,43 @@ Future main() async {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
+  final GoogleSignIn googleSignIn = GoogleSignIn();
+
+  FirebaseUser _currentUser;
+
+  @override
+  void initState() {
+    super.initState();
+
+    FirebaseAuth.instance.onAuthStateChanged.listen((user){
+      _currentUser = user;
+    })
+  }
+
+  Future<FirebaseUser> _getUser() async {
+    if (_currentUser != null)
+      return _currentUser;
+
+    try {
+      final GoogleSignInAccount googleSignInAccount = await googleSignIn.signIn();
+      final GoogleSignInAuthentication googleSignInAuthentication = await googleSignInAccount.authentication;
+
+      final AuthCredential credential = GoogleAuthProvider.getCredential(
+        idToken: googleSignInAuthentication.idToken,
+        accessToken: googleSignInAuthentication.accessToken
+      );
+
+      final AuthResult authResult = await FirebaseAuth.instance.SignInWithCredential(credential);
+
+      final FirebaseUser user = authResult.user;
+
+      return user;
+    }
+    catch (error) {
+      return null;
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
