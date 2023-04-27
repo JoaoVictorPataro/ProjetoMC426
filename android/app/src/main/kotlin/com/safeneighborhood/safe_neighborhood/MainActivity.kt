2d33823baa94ec84fe4f28d8404f2c5c0960981a
Vítor de Melo Calhau <v248740@dac.unicamp.br>
2023-04-27 16:37:45 -0300

    cria estrutura inicial do projeto e configura arquivos necessarios para realizar a ligacao com o Firebase

---
+++ android/app/src/main/kotlin/com/safeneighborhood/safe_neighborhood/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.safeneighborhood.safe_neighborhood
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  runApp(const MyApp());
+
+  // FirebaseFirestore.instance.collection("col").doc("doc").set({"number":1});
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Container(),
+    );
+  }
+}
+
