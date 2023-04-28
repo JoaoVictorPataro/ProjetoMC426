f834ddfe3341906b74a4e617a59e3e04543214c6
Mateus-De-Padua-Vicente <128754846+Mateus-De-Padua-Vicente@users.noreply.github.com>
2023-04-27 21:24:24 -0300

    create signup screen

---
+++ lib/signup_screen.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+
+class SignUpScreen extends StatelessWidget {
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blueAccent,
+        centerTitle: true,
+        title: Text(
+          "Criar conta",
+          style: TextStyle(
+            fontSize: 22,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+        ),
+      ),
+      body: Form(
+        key: _formKey,
+        child: ListView(
+          padding: EdgeInsets.all(16.0),
+          children: <Widget>[
+            TextFormField(
+              decoration: InputDecoration(
+                hintText: "Nome Completo",
+              ),
+              validator: (text) {
+                if (text == "") return "Nome inválido";
+              },
+            ),
+            SizedBox(
+              height: 16.0,
+            ),
+            TextFormField(
+              decoration: InputDecoration(
+                hintText: "E-mail",
+              ),
+              keyboardType: TextInputType.emailAddress,
+              validator: (text) {
+                if (text == "" || text?.contains("@") == false)
+                  return "E-mail inválido";
+              },
+            ),
+            SizedBox(
+              height: 16.0,
+            ),
+            TextFormField(
+              decoration: InputDecoration(
+                hintText: "Endereço",
+              ),
+              validator: (text) {
+                if (text == "") return "Endereço inválida";
+              },
+            ),
+            SizedBox(
+              height: 16.0,
+            ),
+            TextFormField(
+              decoration: InputDecoration(
+                hintText: "Senha",
+              ),
+              obscureText: true,
+              validator: (text) {
+                if (text == "") return "Senha inválida";
+              },
+            ),
+            SizedBox(
+              height: 16.0,
+            ),
+            SizedBox(
+              height: 44.0,
+              child: ElevatedButton(
+                child: Text(
+                  "Criar conta",
+                  style: TextStyle(
+                    fontSize: 18.0,
+                    color: Colors.white,
+                  ),
+                ),
+                onPressed: () {
+                  if (_formKey.currentState?.validate() == true) {}
+                },
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
