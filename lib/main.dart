f801be1b37f5321f83f9f3a0a1081d71da3df38e
Vítor de Melo Calhau <v248740@dac.unicamp.br>
2023-05-04 13:24:42 -0300

    include comments and add const keyword to improve performance

--- lib/main.dart
+++ lib/main.dart
@@ -7,6 +7,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:safe_neighborhood/map.dart';
 
 Future main() async {
+  // inicializa firebase
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
 
@@ -26,7 +27,7 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
-        home: LoginScreen(),
+        home: const LoginScreen(),
       ),
     );
   }
