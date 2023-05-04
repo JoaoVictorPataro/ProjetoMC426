b5180869857ea0efb3dc16c2c855313f97db86ed
Vítor de Melo Calhau <128754342+vitmcalhau@users.noreply.github.com>
2023-05-04 12:42:17 -0300

    fix error by adding missing import

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:google_sign_in/google_sign_in.dart';
 import 'package:safe_neighborhood/login_screen.dart';
 import 'package:safe_neighborhood/models/user_model.dart';
 import 'package:scoped_model/scoped_model.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:safe_neighborhood/map.dart';
 
 Future main() async {
